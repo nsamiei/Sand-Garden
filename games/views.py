ed50f35516c27f5b35e93e24ee026abd9e231653
nima <nimasamiei@hotmail.com>
2016-06-04 05:05:19 -0400

    worked on homepage and games page

--- games/views.py
+++ games/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-#Render the recipe creation template and pass it the list of all recipes
 def index(request):
-    #if not request.user.is_authenticated():
-    #   return redirect('/login')
     return render(request, 'games/index.html')
+	
+def games(request):
+    return render(request, 'games/games.html')
